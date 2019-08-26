bb1793ca1e2dc44d16f6b643f2ee32303100eee4
kisida <shkadsk@yahoo.co.jp>
2019-08-26 22:05:04 +0900

    Incremental-search_create

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,11 +2,12 @@
 $(function() {
 
     var searchuser = $('#user-search-result');
+    var chatuser =   $('.chat-group-users');
 
     function appendUser(user) {
       var html = `<div class="chat-group-user clearfix">
                     <p class="chat-group-user__name">${user.name}</p>
-                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="user.id" data-user-name="user.name">追加</div>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
                   </div>`
       //appendUser(html);
       searchuser.append(html)
@@ -19,6 +20,14 @@ $(function() {
      searchuser.append(html)
   }
 
+  function appendChatUser(chat){
+        var html =`<div class='chat-group-user'>
+                    <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+                    <p class='chat-group-user__name'>${user.name}</p>
+                    <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                   </div>`
+     chatuser.append(html)
+}
   $(function() {
     $("#user-search-field").on("keyup", function() {
       var input = $("#user-search-field").val();
@@ -45,6 +54,18 @@ $(function() {
       .fail(function() {
         alert('ユーザー検索に失敗しました');
       })
+    });
+
+    $(document).on("click", ".user-search-add", function () {
+      console.log(this);
+      thisからidとnameをもらう
+      チャットメンバーに表示する
+      $(this).親要素取得するメソッド.消すメソッド
+
+      // (".chat-group-user").remove()
+    });
+    $(document).on("click", ".chat-group-user__btn", function (chat) {
+      appendChatUser(chat);
     });
   });
 });
