c2292b5066c4829b4aae46d375aa6f7e4099b4cd
kisida <shkadsk@yahoo.co.jp>
2019-08-30 17:10:46 +0900

    Automatic-updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,6 +5,7 @@ $(function(){
       image = `<img class="lower-message__image" src="${message.image.url}" alt="Img 2171">`;
       }
     var html = `<div class="message">
+    ${message.id}
   <div class="upper-info">
     <div class="upper-info__user">
       ${message.user_name}
@@ -48,3 +49,31 @@ $(function(){
   return false;
 })
 })
+$(function() {
+  //省略
+  
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+
+      last_message_id = $('.message:last').data('id')
+     
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url:'api/messages',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        
+        var insertHTML = '';
+
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+    setInterval(reloadMessages, 5000);
+  });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,11 @@
-json.text       @message.content
-json.user_id    @message.user.id
-json.user_name  @message.user.name
-json.day        @message.created_at.strftime("%Y/%m/%d %H:%M")
-json.image      @message.image
+# json.text       @message.content
+# json.user_id    @message.user.id
+# json.user_name  @message.user.name
+# json.day        @message.created_at.strftime("%Y/%m/%d %H:%M")
+# json.image      @message.image
+
+json.(@message, :content, :image)
+json.created_at @message.created_at
+json.user_name @message.user.name
+#idもデータとして渡す
+json.id @message.id
